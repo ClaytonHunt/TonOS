66c536c7e21d61379d82bf1221046ef8c0adca74
Clayton Hunt <ClaytonHunt_104@hotmail.com>
2014-05-16 15:47:59 -0400

    Working kernel example

--- Kernel/Stage3.asm
+++ Kernel/Stage3.asm
@@ -1,14 +1,13 @@
 ; We are still pure binary. We will fix this in the next few tutorials :)
 
-org 0x10000				; Kernel starts at 1MB
+org 0x100000			; Kernel starts at 1MB
 bits 32					; 32 bit code
 
 jmp Stage3								; jump to stage 3
 
 %include "stdio.inc"					; Our stdio.inc file we developed from the previous tutorials
 
-msg db  0x0A, 0x0A, "                       - OS Development Series -"
-    db  0x0A, 0x0A, "                     MOS 32 Bit Kernel Executing", 0x0A, 0
+msg db 0x0A, 0x0A, "Welcome to Kernel Land!!", 0x0A, 0x0A, 0
 
 Stage3:
 	
