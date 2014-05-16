829dd323141701425cfed364149cbd051033be98
Clayton Hunt <ClaytonHunt_104@hotmail.com>
2014-05-16 14:50:48 -0400

    Updated formatting

--- Stage2/stage2.asm
+++ Stage2/stage2.asm
@@ -14,15 +14,15 @@ bits	16
 
 org 0x500
 
-jmp	main				; go to start
+jmp	main						; go to start
 
 ;*******************************************************
 ;	Preprocessor directives
 ;*******************************************************
 
 %include "stdio.inc"			; basic i/o routines
-%include "Gdt.inc"			; Gdt routines
-%include "A20.inc"			; A20 enabling
+%include "Gdt.inc"				; Gdt routines
+%include "A20.inc"				; A20 enabling
 
 ;*******************************************************
 ;	Data Section
@@ -45,46 +45,46 @@ main:
 	;   Setup segments and stack	;
 	;-------------------------------;
 
-	cli				; clear interrupts
-	xor	ax, ax			; null segments
+	cli							; clear interrupts
+	xor	ax, ax					; null segments
 	mov	ds, ax
 	mov	es, ax
-	mov	ax, 0x9000		; stack begins at 0x9000-0xffff
+	mov	ax, 0x9000				; stack begins at 0x9000-0xffff
 	mov	ss, ax
 	mov	sp, 0xFFFF
-	sti				; enable interrupts
+	sti							; enable interrupts
 
-	;-------------------------------;
-	;   Install our GDT		;
-	;-------------------------------;
+	;---------------------------;
+	;   Install our GDT			;
+	;---------------------------;
 
-	call	InstallGDT		; install our GDT
+	call	InstallGDT			; install our GDT
 
-	;-------------------------------;
-	;   Enable A20			;
-	;-------------------------------;
+	;---------------------------;
+	;   Enable A20				;
+	;---------------------------;
 
 	call	EnableA20_KKbrd_Out
 
-	;-------------------------------;
+	;---------------------------;
 	;   Print loading message	;
-	;-------------------------------;
+	;---------------------------;
 
 	mov	si, LoadingMsg
 	call	Puts16
 
-	;-------------------------------;
-	;   Go into pmode		;
-	;-------------------------------;
+	;---------------------------;
+	;   Go into pmode			;
+	;---------------------------;
 
 EnterStage3:
 
-	cli				; clear interrupts
-	mov	eax, cr0		; set bit 0 in cr0--enter pmode
+	cli							; clear interrupts
+	mov	eax, cr0				; set bit 0 in cr0--enter pmode
 	or	eax, 1
 	mov	cr0, eax
 
-	jmp	CODE_DESC:Stage3	; far jump to fix CS
+	jmp	CODE_DESC:Stage3		; far jump to fix CS
 
 	; Note: Do NOT re-enable interrupts! Doing so will triple fault!
 	; We will fix this in Stage 3.
@@ -97,23 +97,23 @@ bits 32
 
 Stage3:
 
-	;-------------------------------;
-	;   Set registers		;
-	;-------------------------------;
+	;---------------------------;
+	;   Set registers			;
+	;---------------------------;
 
 	mov		ax, DATA_DESC		; set data segments to data selector (0x10)
 	mov		ds, ax
 	mov		ss, ax
 	mov		es, ax
-	mov		esp, 90000h		; stack begins from 90000h
+	mov		esp, 90000h			; stack begins from 90000h
 
 	;---------------------------------------;
-	;   Clear screen and print success	;
+	;   Clear screen and print success		;
 	;---------------------------------------;
 
-	call		ClrScr32
+	call	ClrScr32
 	mov		ebx, msg
-	call		Puts32
+	call	Puts32
 
 	;---------------------------------------;
 	;   Stop execution			;
